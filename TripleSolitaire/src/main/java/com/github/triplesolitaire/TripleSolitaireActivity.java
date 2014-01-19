46d2048e47474aad7e523dd659387e5178347f65
Ian Lake <ian.hannibal.lake@gmail.com>
2014-01-18 22:55:19 -0800

    Ensures quick rotations do not cause an exception to be called due to no longer being logged in

--- TripleSolitaire/src/main/java/com/github/triplesolitaire/TripleSolitaireActivity.java
+++ TripleSolitaire/src/main/java/com/github/triplesolitaire/TripleSolitaireActivity.java
@@ -71,7 +71,7 @@ public class TripleSolitaireActivity extends BaseGameActivity implements LoaderC
 
         @Override
         protected void onPostExecute(final Void aVoid) {
-            if (mPendingUpdateState)
+            if (mPendingUpdateState && isSignedIn())
                 saveToCloud();
         }
     };
