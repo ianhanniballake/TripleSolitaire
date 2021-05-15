e233da6611f772f1d96ce505406a960f549aa55a
Ian Lake <ian.hannibal.lake@gmail.com>
2021-05-15 16:36:15 -0700

    Convert WinDialogFragment to Kotlin

--- mobile/src/main/java/com/github/triplesolitaire/WinDialogFragment.kt
+++ mobile/src/main/java/com/github/triplesolitaire/WinDialogFragment.kt
@@ -1,69 +1,64 @@
-package com.github.triplesolitaire;
+package com.github.triplesolitaire
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.content.Intent
+import android.os.Bundle
+import androidx.core.os.bundleOf
 
 /**
  * Dialog to show when a user wins the game
  */
-public class WinDialogFragment extends DialogFragment {
-    private static final String GAME_WIN_TIME = "GAME_WIN_TIME";
-    private static final String GAME_WIN_MOVE_COUNT = "GAME_WIN_MOVE_COUNT";
-
-    /**
-     * Create a new Intent which can be passed via setResult(int requestCode, Intent data) to eventually create a
-     * WinDialogFragment
-     *
-     * @param time      time of the winning game
-     * @param moveCount move count of the winning game
-     * @return Intent suitable for setResult(int requestCode, Intent data)
-     */
-    public static Intent createDataIntent(CharSequence time, int moveCount) {
-        Intent data = new Intent();
-        data.putExtra(GAME_WIN_TIME, time);
-        data.putExtra(GAME_WIN_MOVE_COUNT, moveCount);
-        return data;
-    }
-
-    /**
-     * Create a new instance of WinDialogFragment given the Intent created by createDataIntent
-     *
-     * @param data Intent created by createDataIntent()
-     * @return A valid instance of WinDialogFragment
-     */
-    public static WinDialogFragment createInstance(final Intent data) {
-        WinDialogFragment winDialogFragment = new WinDialogFragment();
-        Bundle args = new Bundle();
-        args.putCharSequence(GAME_WIN_TIME, data.getCharSequenceExtra(GAME_WIN_TIME));
-        args.putInt(GAME_WIN_MOVE_COUNT, data.getIntExtra(GAME_WIN_MOVE_COUNT, 0));
-        winDialogFragment.setArguments(args);
-        return winDialogFragment;
-    }
-
-    @Override
-    public Dialog onCreateDialog(final Bundle savedInstanceState) {
-        final TripleSolitaireActivity activity = (TripleSolitaireActivity) getActivity();
-        final CharSequence time = getArguments().getCharSequence(GAME_WIN_TIME);
-        final int moveCount = getArguments().getInt(GAME_WIN_MOVE_COUNT);
-        final String message = getString(R.string.win_dialog, time, moveCount);
-        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+class WinDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+        val activity = activity as TripleSolitaireActivity
+        val time = arguments.getCharSequence(GAME_WIN_TIME)
+        val moveCount = arguments.getInt(GAME_WIN_MOVE_COUNT)
+        val message = getString(R.string.win_dialog, time, moveCount)
+        val builder = AlertDialog.Builder(activity)
         builder.setMessage(message).setCancelable(false)
-                .setPositiveButton(getString(R.string.new_game), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(final DialogInterface dialog, final int dialogId) {
-                        dialog.dismiss();
-                        activity.newGame();
-                    }
-                }).setNegativeButton(getString(R.string.main_menu), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(final DialogInterface dialog, final int dialogId) {
-                dialog.dismiss();
+            .setPositiveButton(getString(R.string.new_game)) { dialog, _ ->
+                dialog.dismiss()
+                activity.newGame()
             }
-        });
-        return builder.create();
+            .setNegativeButton(getString(R.string.main_menu)) { dialog, _ -> dialog.dismiss() }
+        return builder.create()
+    }
+
+    companion object {
+        private const val GAME_WIN_TIME = "GAME_WIN_TIME"
+        private const val GAME_WIN_MOVE_COUNT = "GAME_WIN_MOVE_COUNT"
+
+        /**
+         * Create a new Intent which can be passed via setResult(int requestCode, Intent data)
+         * to eventually create a WinDialogFragment
+         *
+         * @param time      time of the winning game
+         * @param moveCount move count of the winning game
+         * @return Intent suitable for setResult(int requestCode, Intent data)
+         */
+        @JvmStatic
+        fun createDataIntent(time: CharSequence, moveCount: Int) = Intent().apply {
+            putExtra(GAME_WIN_TIME, time)
+            putExtra(GAME_WIN_MOVE_COUNT, moveCount)
+        }
+
+        /**
+         * Create a new instance of WinDialogFragment given the Intent created by createDataIntent
+         *
+         * @param data Intent created by createDataIntent()
+         * @return A valid instance of WinDialogFragment
+         */
+        @JvmStatic
+        fun createInstance(data: Intent): WinDialogFragment {
+            val winDialogFragment = WinDialogFragment()
+            val args = bundleOf(
+                GAME_WIN_TIME to data.getCharSequenceExtra(GAME_WIN_TIME),
+                GAME_WIN_MOVE_COUNT to data.getIntExtra(GAME_WIN_MOVE_COUNT, 0)
+            )
+            winDialogFragment.arguments = args
+            return winDialogFragment
+        }
     }
 }
