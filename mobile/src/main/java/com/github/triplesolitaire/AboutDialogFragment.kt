c4590c90365e697d1b9410c92e26ba156e1c11c8
Ian Lake <ian.hannibal.lake@gmail.com>
2021-05-15 16:16:22 -0700

    Convert AboutDialogFragment to Kotlin

--- mobile/src/main/java/com/github/triplesolitaire/AboutDialogFragment.kt
+++ mobile/src/main/java/com/github/triplesolitaire/AboutDialogFragment.kt
@@ -1,23 +1,20 @@
-package com.github.triplesolitaire;
+package com.github.triplesolitaire
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.os.Bundle
 
 /**
  * About Dialog for the application
  */
-public class AboutDialogFragment extends DialogFragment {
-    @Override
-    public Dialog onCreateDialog(final Bundle savedInstanceState) {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        final LayoutInflater inflater = getActivity().getLayoutInflater();
-        final View layout = inflater.inflate(R.layout.about_dialog, null);
+class AboutDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+        val builder = AlertDialog.Builder(activity)
+        val inflater = activity.layoutInflater
+        val layout = inflater.inflate(R.layout.about_dialog, null)
         builder.setTitle(R.string.app_name).setIcon(R.mipmap.ic_launcher).setView(layout)
-                .setNegativeButton(getText(R.string.close), null);
-        return builder.create();
+            .setNegativeButton(getText(R.string.close), null)
+        return builder.create()
     }
 }
