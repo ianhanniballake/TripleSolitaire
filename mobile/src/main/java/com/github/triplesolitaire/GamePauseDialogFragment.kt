eb14f8c49595dd3320826774f3b66fcf0599e7e6
Ian Lake <ian.hannibal.lake@gmail.com>
2021-05-15 16:39:57 -0700

    Convert GamePauseDialogFragment to Kotlin

--- mobile/src/main/java/com/github/triplesolitaire/GamePauseDialogFragment.kt
+++ mobile/src/main/java/com/github/triplesolitaire/GamePauseDialogFragment.kt
@@ -1,19 +1,19 @@
-package com.github.triplesolitaire;
+package com.github.triplesolitaire
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.os.Bundle;
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.os.Bundle
 
 /**
  * Dialog to show when a user pauses the game
  */
-public class GamePauseDialogFragment extends DialogFragment {
-    @Override
-    public Dialog onCreateDialog(final Bundle savedInstanceState) {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.app_name).setMessage(R.string.game_paused)
-                .setNegativeButton(getText(R.string.resume), null);
-        return builder.create();
-    }
+class GamePauseDialogFragment : DialogFragment() {
+    override fun onCreateDialog(
+        savedInstanceState: Bundle
+    ): Dialog = AlertDialog.Builder(activity)
+        .setTitle(R.string.app_name)
+        .setMessage(R.string.game_paused)
+        .setNegativeButton(getText(R.string.resume), null)
+        .create()
 }
