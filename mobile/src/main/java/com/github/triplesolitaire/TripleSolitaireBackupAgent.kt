cc3e26a11ac7fcb3d4b8eac059c7f842b73779ae
Ian Lake <ian.hannibal.lake@gmail.com>
2021-05-15 16:13:26 -0700

    Convert TripleSolitaireBackupAgent to Kotlin

--- mobile/src/main/java/com/github/triplesolitaire/TripleSolitaireBackupAgent.kt
+++ mobile/src/main/java/com/github/triplesolitaire/TripleSolitaireBackupAgent.kt
@@ -1,25 +1,22 @@
-package com.github.triplesolitaire;
+package com.github.triplesolitaire
 
-import android.app.backup.BackupAgentHelper;
-import android.app.backup.SharedPreferencesBackupHelper;
+import android.app.backup.BackupAgentHelper
+import android.app.backup.SharedPreferencesBackupHelper
 
 /**
  * Agent which handles backup of user settings
  */
-public class TripleSolitaireBackupAgent extends BackupAgentHelper {
+class TripleSolitaireBackupAgent : BackupAgentHelper() {
     /**
      * Gets the name associated with PreferenceManager.getDefaultPreferences(), as given by the Android source code
      *
      * @return The name associated with PreferenceManager.getDefaultPreferences()
      */
-    public String getDefaultSharedPreferencesName() {
-        return getPackageName() + "_preferences";
-    }
+    private val defaultSharedPreferencesName: String
+        get() = packageName + "_preferences"
 
-    @Override
-    public void onCreate() {
-        final SharedPreferencesBackupHelper helper = new SharedPreferencesBackupHelper(this,
-                getDefaultSharedPreferencesName());
-        addHelper("preferences", helper);
+    override fun onCreate() {
+        val helper = SharedPreferencesBackupHelper(this, defaultSharedPreferencesName)
+        addHelper("preferences", helper)
     }
 }
